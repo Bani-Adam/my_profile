5703631acbd6bdef6d3364da387c56eae35e0f65
Bani-Adam <042528546@ecampus.ut.ac.id>
2023-05-16 11:15:58 +0700

    first commit

---
+++ src/views/AboutView.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="text-center">
+        <h2 class="text-center text-xl sm:text-2xl font-bold uppercase dark:text-white">Hi! Saya Bani Adam </h2>
+        <p class="sm:text-center text-justify my-4 sm:w-4/12 mx-auto dark:text-gray-300">
+            Saya mahasiswa Sistem Informasi - Universitas Terbuka, 
+            seorang Front-End Web and Mobile Developer(Junior) dan 
+            saya pecinta kopi</p>
+        <p class="text-center text-blue-600">#Front-EndDeveloper #MobileDeveloper</p>
+    </div>
+</template>
+<script setup>
+
+
+</script>
