7a0f4c50c4abf3ce2f6a9b09705a0dc0637f37c3
Bani-Adam <042528546@ecampus.ut.ac.id>
2023-10-06 14:06:02 +0700

    Update

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,7 +2,7 @@
     <div class="text-center">
         <h2 class="text-center text-xl sm:text-2xl font-bold uppercase dark:text-white">Hi! Saya Bani Adam </h2>
         <p class="sm:text-center text-justify my-4 sm:w-4/12 mx-auto dark:text-gray-300">
-            Mahasiswa Sistem Informasi - Universitas Terbuka, seorang Front - End Web and Mobile Developer(Junior), 
+            Mahasiswa Sistem Informasi - Universitas Terbuka, seorang Front-End Web and Mobile Developer (Junior), 
             Teknisi Perangkat Mobile dan Desktop, Konten Kreator, pecinta kopi dan berasal dari Kota Bandar Lampung</p>
     </div>
 </template>
